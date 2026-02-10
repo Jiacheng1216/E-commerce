951dca2086f9a4a35fe605c182a2cc90076fa5c4
Eric.Lin <Eric.Lin@skylinetw.com>
2026-02-11 01:19:48 +0800

    fix：刪除bcrypt

--- server/models/user-models.js
+++ server/models/user-models.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   username: {
@@ -27,27 +27,27 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.methods.comparePassword = async function (password, cb) {
-  let result;
-  try {
-    //result的值為true or false
-    result = await bcrypt.compare(password, this.password);
-    return cb(null, result);
-  } catch (e) {
-    return cb(e, result);
-  }
-};
+// userSchema.methods.comparePassword = async function (password, cb) {
+//   let result;
+//   try {
+//     //result的值為true or false
+//     result = await bcrypt.compare(password, this.password);
+//     return cb(null, result);
+//   } catch (e) {
+//     return cb(e, result);
+//   }
+// };
 
-// mongoose mmiddlewares
-// 若使用者為新用戶，或是正在更改密碼，則將密碼進行雜湊處理
-userSchema.pre("save", async function (next) {
-  //this代表mongoDB的Document
-  if (this.isNew || this.isModified("password")) {
-    //將密碼進行雜湊處理
-    const hashValue = await bcrypt.hash(this.password, 10);
-    this.password = hashValue;
-  }
-  next();
-});
+// // mongoose mmiddlewares
+// // 若使用者為新用戶，或是正在更改密碼，則將密碼進行雜湊處理
+// userSchema.pre("save", async function (next) {
+//   //this代表mongoDB的Document
+//   if (this.isNew || this.isModified("password")) {
+//     //將密碼進行雜湊處理
+//     const hashValue = await bcrypt.hash(this.password, 10);
+//     this.password = hashValue;
+//   }
+//   next();
+// });
 
 module.exports = mongoose.model("User", userSchema);
