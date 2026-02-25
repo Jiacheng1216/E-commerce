aadd2571d5740549e564210751856b6bde08ed68
Eric.Lin <Eric.Lin@skylinetw.com>
2026-02-25 16:01:42 +0800

    feat：footer元件

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./NavComponent/nav-component.jsx";
+import FooterComponent from "./footerComponent/FooterComponent.tsx";
 
 const Layout = ({ currentUser, setCurrentUser }) => {
   return (
     <>
       <Nav currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <Outlet />
+      <FooterComponent></FooterComponent>
     </>
   );
 };
---
+++ client/src/components/footerComponent/FooterComponent.tsx
@@ -0,0 +1,11 @@
+import "./FooterComponent.css"
+
+const FooterComponent = () => {
+    return(
+        <footer>
+            <p>© 2023-2026 JC Lin 電商平台 All rights reserved. </p>
+        </footer>
+    );
+};
+
+export default FooterComponent
