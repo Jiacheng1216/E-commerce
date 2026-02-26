51c112cc2e1587b73421637700bcb7725a12983c
Eric.Lin <Eric.Lin@skylinetw.com>
2026-02-26 16:41:57 +0800

    fix：更新物品一覽UI

--- client/src/components/Item-component/Item-component.jsx
+++ client/src/components/Item-component/Item-component.jsx
@@ -23,68 +23,40 @@ const ItemComponent = ({ currentUser, setCurrentUser }) => {
   };
 
   return (
-    <div style={{ padding: "3rem" }}>
-      {!currentUser && (
-        <div>
-          <p className="hint">進行登入以開始購買商品</p>
-        </div>
-      )}
-      <div className="spil">
-        <h1>商品一覽</h1>
-      </div>
+    <div className="itemPage">
+      {!currentUser && <h1 className="hint">進行登入以開始購買商品</h1>}
+      <h1 className="itemPageH1">商品一覽</h1>
 
       {itemData && itemData.length != 0 && (
-        <div
-          className="card-container"
-          style={{ display: "flex", flexWrap: "wrap" }}
-        >
+        <div className="itemCard-container">
           {itemData.map((item) => {
             return (
-              <div className="card-group" key={item._id}>
-                <Link to={`IndividualItem/${item._id}`} className="link-style">
-                  <div
-                    className="card shadow-on-hover"
-                    style={{ width: "18rem", margin: "1rem" }}
-                  >
-                    <img
-                      src={`http://localhost:8080/images/${item.imagePath}`}
-                      className="card-img-top"
-                      alt="..."
-                      height={250}
-                    ></img>
-                    <div className="card-body">
-                      <h4 className="card-title">
-                        由 {item.seller.sellerName} 刊登
-                      </h4>
-                      <h5 className="card-title">{item.title}</h5>
-                      <p
-                        style={{ margin: "0.5rem 0rem" }}
-                        className="card-text description"
-                      >
-                        {item.description}
-                      </p>
-                      <p
-                        style={{
-                          margin: "0.5rem 0rem",
-                          color: "red",
-                        }}
-                      >
-                        $ {item.price}
-                      </p>
-                      <p style={{ margin: "0.5rem 0rem" }}>
-                        剩餘: {item.quantity} 個
-                      </p>
-                      <p style={{ margin: "0.5rem 0rem" }}>
-                        {item.date[0]}
-                        {item.date[1]}
-                        {item.date[2]}
-                        {item.date[3]} 年 {item.date[5]}
-                        {item.date[6]} 月 {item.date[8]}
-                        {item.date[9]} 日 {item.date[11]}
-                        {item.date[12]} : {item.date[14]}
-                        {item.date[15]} 上傳
-                      </p>
-                    </div>
+              <div className="itemCard" key={item._id}>
+                <Link to={`IndividualItem/${item._id}`}>
+                  <img
+                    src={`http://localhost:8080/images/${item.imagePath}`}
+                    className="card-img"
+                    alt="..."
+                  ></img>
+                  <div className="card-body">
+                    <h2 className="card-text-title">{item.title}</h2>
+                    <span className="card-text-price">$ {item.price}</span>
+                    <span className="card-text-author">
+                      由 {item.seller.sellerName} 刊登
+                    </span>
+                    <span className="card-text-date">
+                      {item.date[0]}
+                      {item.date[1]}
+                      {item.date[2]}
+                      {item.date[3]} 年 {item.date[5]}
+                      {item.date[6]} 月 {item.date[8]}
+                      {item.date[9]} 日 {item.date[11]}
+                      {item.date[12]} : {item.date[14]}
+                      {item.date[15]} 上傳
+                    </span>
+                    <span className="card-text-quantity">
+                      剩餘: {item.quantity} 個
+                    </span>
                   </div>
                 </Link>
               </div>
