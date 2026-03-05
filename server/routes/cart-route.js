d05d9ce5e2735ee4bec5e7b61fb0b9a0bf9b98fb
jiacheng1216 <ah881216@gmail.com>
2026-03-05 15:15:11 +0800

    fix：重寫後端更新購物車的路由

--- server/routes/cart-route.js
+++ server/routes/cart-route.js
@@ -6,54 +6,86 @@ router.use((req, res, next) => {
   next();
 });
 
-// 增加商品到購物車的router
+// 更新購物車的路由
 router.post("/", async (req, res) => {
-  try {
-    let { userID, itemID, quantity, imagePath, title, price } = req.body;
-    let postCart = new Cart({
-      userID,
-      itemID,
-      quantity,
-      imagePath,
-      title,
-      price,
-    });
+  const { userID, itemID, quantity } = req.body;
 
-    let savedCart = await postCart.save();
-    return res.send({
-      msg: "成功將商品加入到購物車",
-      savedCart,
-    });
+  try {
+    let cart = await Cart.findOne({userId:userID});
+
+    if(!cart || cart == undefined){
+      let cart = new Cart({
+        userId:userID,
+        items:[{itemId:itemID,quantity:quantity}]
+      })
+
+      await cart.save();
+      return res.status(200).send(
+        {
+          msg: "成功新增購物車資料",
+          cart,  
+        }
+      )
+    }
+
+    const itemIndex = cart.items.findIndex(i => i.itemId.toString() === itemID)
+
+    if(itemIndex > -1){
+      cart.items[itemIndex].quantity += Number(quantity);
+
+      if(quantity == 0){
+        cart.items.splice(itemIndex,1);
+        await cart.save();
+        return res.status(203).send(
+          {
+            msg: "成功從購物車中刪除商品",
+            cart,  
+          }
+        )
+      }
+
+      await cart.save();
+      return res.status(202).send({
+        msg: "成功從購物車中更新商品數量",
+        cart,  
+      })
+    }else{
+      cart.items.push({itemId:itemID,quantity:quantity})
+      return res.status(201).send({
+        msg: "成功從購物車中新增商品",
+        cart,  
+      })
+    }
   } catch (e) {
-    return res.status(500).send("無法將商品加入到購物車...");
+    return res.status(500).send("無法更新購物車資料...");
   }
 });
 
 //以訂單id刪除購物車商品
-router.delete("/delete/:id", async (req, res) => {
-  try {
-    const cartID = req.params.id;
-    const deleteCart = await Cart.findByIdAndDelete(cartID);
-    if (!deleteCart) return res.status(404).json({ error: "訂單未找到" });
-    return res.send({
-      msg: "刪除訂單成功",
-      deleteCart,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("刪除購物車商品時出錯");
-  }
-});
+// router.delete("/delete/:id", async (req, res) => {
+//   try {
+//     const cartID = req.params.id;
+//     const deleteCart = await Cart.findByIdAndDelete(cartID);
+//     if (!deleteCart) return res.status(404).json({ error: "訂單未找到" });
+//     return res.send({
+//       msg: "刪除訂單成功",
+//       deleteCart,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).send("刪除購物車商品時出錯");
+//   }
+// });
 
 //查找資料庫中所有訂單
-router.get("/showCart", async (req, res) => {
-  try {
-    const allCart = await Cart.find();
-    return res.send(allCart);
-  } catch (e) {
-    res.status(500).send("無法查詢所有訂單");
-  }
-});
+// router.get("/showCart", async (req, res) => {
+//   try {
+//     const allCart = await Cart.find();
+//     return res.send(allCart);
+//   } catch (e) {
+//     res.status(500).send("無法查詢所有訂單");
+//   }
+// });
 
 //以用戶id來查詢購物車中的商品
 router.get("/selfCart/:id", async (req, res) => {
@@ -67,21 +99,22 @@ router.get("/selfCart/:id", async (req, res) => {
 });
 
 //以訂單id更新訂單資訊
-router.put("/edit/:id", async (req, res) => {
-  const cartID = req.params.id;
-  const { quantity } = req.body;
+// router.put("/edit/:id", async (req, res) => {
+//   const cartID = req.params.id;
+//   const { quantity } = req.body;
 
-  try {
-    const cart = await Cart.findById(cartID);
-    if (!cartID) return res.status(404).send("訂單未找到");
+//   try {
+//     const cart = await Cart.findById(cartID);
+//     if (!cartID) return res.status(404).send("訂單未找到");
 
-    cart.quantity = quantity;
+//     cart.quantity = quantity;
 
-    const updatedCart = await cart.save();
-    res.status(200).send({ msg: "訂單資訊更新成功!", updatedCart });
-  } catch (e) {
-    res.status(500).send("無法更新訂單資訊");
-  }
-});
+//     const updatedCart = await cart.save();
+//     res.status(200).send({ msg: "訂單資訊更新成功!", updatedCart });
+//   } catch (e) {
+//     res.status(500).send("無法更新訂單資訊");
+//   }
+// });
 
 module.exports = router;
+
