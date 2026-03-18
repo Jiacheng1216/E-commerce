b5126fd172bd7dc9bc7bb2e293c847a06d125def
jiacheng1216 <ah881216@gmail.com>
2026-03-18 13:31:07 +0800

    feat：前端新增搜尋頁面

---
+++ client/src/components/searchComponent/searchComponent.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ItemService from "../../services/item.service";
+import "./searchComponent.css";
+
+const SearchComponent = ({ currentUser, setCurrentUser }) => {
+  return <div></div>;
+};
+
+export default SearchComponent;
